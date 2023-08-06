05e300056ecce575b7f15fe2e25641b7190f9496
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2023-08-06 23:21:34 +0200

    WIP

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,10 +3,16 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'se.khromov.starter',
   appName: 'SvelteKit Starter',
-  webDir: 'dist',
+  webDir: 'build',
+  /*
   server: {
     androidScheme: 'https'
   }
+  */
+  server: {
+		url: 'http://192.168.10.86:5173/',
+		cleartext: true
+	}
 };
 
 export default config;
