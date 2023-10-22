e7fd00120d1c8b044d32760cbf93c04ed8adf4e4
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2023-10-22 23:52:15 +0200

    Use dynamic import

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,8 @@
 import type { PageLoad } from './$types';
-import { JeepSqlite } from 'jeep-sqlite/dist/components/jeep-sqlite';
 
 export const load: PageLoad = async ({ params }) => {
-    customElements.define('jeep-sqlite', JeepSqlite);
+	const i = await import('jeep-sqlite/dist/components/jeep-sqlite');
+    customElements.define('jeep-sqlite', i.JeepSqlite);
 	const jeepEl = document.createElement("jeep-sqlite");
 	document.body.appendChild(jeepEl);
 	const res = await customElements.whenDefined('jeep-sqlite');
