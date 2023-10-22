60b8185e7693b05f7d712c83ae2f22f760717925
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2023-10-22 03:49:28 +0200

    Disable SSR

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,6 +3,10 @@ import { JeepSqlite } from 'jeep-sqlite/dist/components/jeep-sqlite';
 
 export const load: PageLoad = async ({ params }) => {
     customElements.define('jeep-sqlite', JeepSqlite);
+	const jeepEl = document.createElement("jeep-sqlite");
+	document.body.appendChild(jeepEl);
+	const res = await customElements.whenDefined('jeep-sqlite');
+	console.log('Init JeepSqlite');
 
 	return {
 		hello: 'world',
